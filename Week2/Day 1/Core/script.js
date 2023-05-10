97c69eeedbc078b2abfc51315e7abd4bfbf51fef
MariemSo <souayah.mariem@gmail.com>
2023-05-10 21:28:47 +0100

    js functions

--- Week2/Day 1/Core/script.js
+++ Week2/Day 1/Core/script.js
@@ -1,6 +1,14 @@
-
+// login to logout
 function changeT(e){
  e.innerHTML ="logout";
  console.log(e)
 }
 
+// remove button
+function remove(){
+    var x = document.querySelector(".addButton");
+x.remove();}
+
+// alert when liked
+function likedNot(){
+alert("ninja was liked");}
